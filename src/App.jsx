eeac353c64bfd7014c99a93b4dc2ab37c8b04b61
Masoudi00 <medelwork@gmail.com>
2023-11-05 15:35:49 +0100

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {useEffect} from 'react'
+// 56969659
+
+const API_URL = 'http://www.omdbapi.com?apikey=56969659'
+
+const App = () =>  {
+
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data) ;
+}
+
+useEffect(() => {
+    searchMovies('Spiderman');
+}, [])
+
+  return (
+    <h1>App</h1>
+  );
+
+}
+
+export default App
